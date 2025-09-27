eadf8b1deda929db0ee168d122f7e74638c8a088
saurav2520 <sauravkumar70799@gmail.com>
2025-09-27 18:08:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## ⚙️ Getting Started
+
+Follow these steps to get the project up and running on your local machine.
+
+### Prerequisites
+
+You must have [Node.js](https://nodejs.org/) and npm installed.
+
+### Installation & Running
+
+1.  **Clone the repository (or create the files manually):**
+    ```sh
+    git clone [https://your-repo-url.com/](https://your-repo-url.com/)
+    cd your-project-folder
+    ```
+
+2.  **Install dependencies:**
+    This will install Gulp and all necessary plugins listed in `package.json`.
+    ```sh
+    npm install
+    ```
+
+3.  **Run the development server:**
+    This command executes the default Gulp task, which builds the site, starts a local server, and watches for any file changes.
+    ```sh
+    npm start
+    ```
+
+The site will be available at `http://localhost:3000`.
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const browserSync = require('browser-sync').create();
+
+function nunjucksTask() {
+  return gulp.src('src/templates/pages/**/*.njk')
+    .pipe(nunjucksRender({
+      // THIS IS THE CRUCIAL LINE THAT FIXES THE ERROR
+      path: ['src/templates'] 
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+}
+
+// Task to copy assets
+function assetsTask() {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'))
+    .pipe(browserSync.stream());
+}
+
+// Task to serve the site and watch for changes
+function serveTask() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    port: 3000
+  });
+
+  gulp.watch('src/templates/**/*.njk', nunjucksTask);
+  gulp.watch('src/assets/**/*', assetsTask);
+}
+
+// Default Gulp task
+exports.default = gulp.series(
+  gulp.parallel(nunjucksTask, assetsTask),
+  serveTask
+);
+
+// Build task for production
+exports.build = gulp.parallel(nunjucksTask, assetsTask);
+
+// Your default task remains the same for development
+exports.default = gulp.series(
+  exports.build,
+  serveTask
+);
